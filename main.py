a32a2263b41ec22652dc8061d9d2d2bfe3f6d88d
Md Asraful Alom <malom@luc.edu>
2014-12-16 11:30:17 -0600

    basic file upload fixed

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 #!venv/bin/python
-from flask import Flask
+
+from flask import Flask, request
+from werkzeug import secure_filename
+
+import os
+import config
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return "I am running!"
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in config.ALLOWED_EXTENSIONS
+
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return filename
+
+    return ''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
