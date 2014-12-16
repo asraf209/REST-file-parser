85f310acae4e2b1f0ac2e9776d28f5af592ce640
Md Asraful Alom <malom@luc.edu>
2014-12-16 13:38:15 -0600

    counting works

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from werkzeug import secure_filename
 
 import os
 import config
-import filereader
+import parser
 
 
 app = Flask(__name__)
@@ -29,7 +29,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return filereader.count_lines(filename)
+            return parser.parse(filename)
 
     return ''
 
--- parser.py
+++ parser.py
@@ -1,13 +1,24 @@
 import config
 
-def count_lines(filename):
+map = {}
+
+def parse(filename):
     num_lines = 0
     num_words = 0
 
     with open(config.UPLOAD_FOLDER + '/' + filename, 'r') as f:
         for line in f:
-            words = line.split()
-
             num_lines += 1
+            words = line.split()
             num_words += len(words)
-        return str(num_lines) + ', ' + str(num_words)
+            populate_map(words)
+        return str(map)
+        #return str(num_lines) + ', ' + str(num_words)
+
+
+def populate_map(words):
+    for word in words:
+        if map.get(word) is None:
+            map[word] = 1
+        else:
+            map[word] = map.get(word) + 1
