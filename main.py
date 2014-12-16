4bd627301ac28e804b1eaf5313803834e72f615f
Md Asraful Alom <malom@luc.edu>
2014-12-16 11:22:42 -0600

    REST is running for home page

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!venv/bin/python
+from flask import Flask
 
+app = Flask(__name__)
 
-
+@app.route('/')
+def index():
+    return "I am running!"
 
 if __name__ == '__main__':
     app.run(debug=True)
