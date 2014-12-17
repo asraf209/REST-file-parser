0beeae2cb280f32ee99050f6c89cf24f22b9aba7
Md Asraful Alom <malom@luc.edu>
2014-12-17 01:22:49 -0600

    single file info worls

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
-from os import listdir
+
+from os import listdir, stat
 from os.path import join, isfile
+from time import asctime, localtime
 
 import config
 import parser
+import json
 
 
 app = Flask(__name__)
@@ -53,7 +56,32 @@ def upload_file():
 @app.route('/files/', methods=['GET'])
 def get_all_files():
     files = [ f for f in listdir(config.UPLOAD_FOLDER) if isfile(join(config.UPLOAD_FOLDER, f)) ]
-    return '\n'.join(files)
+    return 'All uploaded files: \n' + '\n'.join(files)
+
+
+
+
+# Returns detail info of an uploaded file
+@app.route('/files/<filename>', methods=['GET'])
+def get_file(filename):
+    if isfile(config.UPLOAD_FOLDER + '/' + filename):
+        try:
+            st = stat(config.UPLOAD_FOLDER + '/' + filename)
+
+            out = {}
+            out['File Name'] = filename
+            out['Size'] = st.st_size
+            out['Last Access Time'] = asctime(localtime(st.st_atime))
+            out['Last Modify Time'] = asctime(localtime(st.st_mtime))
+
+            return json.dumps(out, indent = 4, separators=(',', ': '),sort_keys=True)
+
+        except:
+            return 'Failed to get information about the file: ' + filename
+    else:
+        return 'File not found: ' + filename + '\n'
+
+
 
 
 
