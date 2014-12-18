5901d3651aa6c04434f9bc1c1c19bc7a2765a101
Md Asraful Alom <malom@luc.edu>
2014-12-18 15:19:07 -0600

    test root passed

--- fileparser_test.py
+++ fileparser_test.py
@@ -1,17 +1,25 @@
 #import os
-import main
+import fileparser
 import unittest
 
 class FileParserTestCase(unittest.TestCase):
 
     def setUp(self):
-        main.app.config['TESTING'] = True
-        self.app = main.app.test_client()
+        fileparser.app.config['TESTING'] = True
+        self.app = fileparser.app.test_client()
 
     # Test Root of the application
-    def test_home_page(self):
-        rv = self.app.get('/')
-        assert "I am running!" in rv.data
+    def test_root(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        assert "I am running!" in response.data
+
+
+    """def test_all_files(self):
+        response = self.app.get('/files')
+        self.assertEqual(response.status_code, 200)
+        print response.data
+    """
 
 if __name__ == '__main__':
     unittest.main()
