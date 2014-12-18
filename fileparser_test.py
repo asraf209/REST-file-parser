3014e7ebc7e09fb893ae1f210238b008268e85b6
Md Asraful Alom <malom@luc.edu>
2014-12-18 14:27:10 -0600

    index page testing passed

--- fileparser_test.py
+++ fileparser_test.py
@@ -8,5 +8,10 @@ class FileParserTestCase(unittest.TestCase):
         main.app.config['TESTING'] = True
         self.app = main.app.test_client()
 
+    # Test Root of the application
+    def test_home_page(self):
+        rv = self.app.get('/')
+        assert "I am running!" in rv.data
+
 if __name__ == '__main__':
     unittest.main()
