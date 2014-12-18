7c633c1b576168a9d5927a328cdf3641811de7f4
Md Asraful Alom <malom@luc.edu>
2014-12-18 17:35:04 -0600

    refactoring

--- test_fileparser.py
+++ test_fileparser.py
@@ -11,28 +11,32 @@ class FileParserTestCase(unittest.TestCase):
         fileparser.app.config['TESTING'] = True
         self.app = fileparser.app.test_client()
 
+
     # Test '404 Not Found'
     def test_url_not_found(self):
         response = self.app.get('/foo')
         self.assertFalse(response.status_code == 200)
         self.assertTrue(response.status_code == 404, msg='URL Not Found')
 
+
     # Test Root of the application
     def test_root(self):
         response = self.app.get('/')
-        self.assertTrue(response.status_code == 200, msg='Page loads successfully')
+        self.assertTrue(response.status_code == 200, msg='App works fine')
         self.assertTrue("I am running!" in response.data)
+
 
     # Test uploading a file
     def test_upload(self):
         response = self.app.post('/upload',
                     data={'file':(StringIO('We are sample contents'), 'test.txt')})
-        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.status_code == 200, msg='App works fine')
 
         data = json.loads(response.data)
         self.assertEqual(data['File Name'], 'test.txt')
         self.assertEqual(data['Number of Lines'], 1)
         self.assertEqual(data['Number of Words'], 4)
+
 
     # Test uploading a blank file
     def test_upload_a_blank_file(self):
