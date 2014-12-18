c3b555f1a73ed51b251712b2a175887aaa08dbf2
Md Asraful Alom <malom@luc.edu>
2014-12-18 17:29:40 -0600

    test blank file and refactoring

--- test_fileparser.py
+++ test_fileparser.py
@@ -1,3 +1,5 @@
+#!venv/bin/python
+
 import unittest
 import fileparser
 from StringIO import StringIO
@@ -9,11 +11,17 @@ class FileParserTestCase(unittest.TestCase):
         fileparser.app.config['TESTING'] = True
         self.app = fileparser.app.test_client()
 
+    # Test '404 Not Found'
+    def test_url_not_found(self):
+        response = self.app.get('/foo')
+        self.assertFalse(response.status_code == 200)
+        self.assertTrue(response.status_code == 404, msg='URL Not Found')
+
     # Test Root of the application
     def test_root(self):
         response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        assert "I am running!" in response.data
+        self.assertTrue(response.status_code == 200, msg='Page loads successfully')
+        self.assertTrue("I am running!" in response.data)
 
     # Test uploading a file
     def test_upload(self):
@@ -26,13 +34,17 @@ class FileParserTestCase(unittest.TestCase):
         self.assertEqual(data['Number of Lines'], 1)
         self.assertEqual(data['Number of Words'], 4)
 
-
-
-    """def test_all_files(self):
-        response = self.app.get('/files')
+    # Test uploading a blank file
+    def test_upload_a_blank_file(self):
+        response = self.app.post('/upload',
+                    data={'file':(StringIO(''), 'test-blank.txt')})
         self.assertEqual(response.status_code, 200)
-        print response.data
-    """
+
+        data = json.loads(response.data)
+        self.assertEqual(data['File Name'], 'test-blank.txt')
+        self.assertEqual(data['Number of Lines'], 0)
+        self.assertEqual(data['Number of Words'], 0)
+
 
 if __name__ == '__main__':
     unittest.main()
